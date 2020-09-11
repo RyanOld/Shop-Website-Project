0edff16456d69454aa5e32854502b79619b0555f
Aziz Nurhidayat <aziznurhidayata@gmail.com>
2020-09-11 19:45:26 +0700

    test

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var objectSelector = document.querySelector(".")
+console.log("test");
+function addNumbers(a, b) {
+  var result;
+  a > b ? result = a : result = b;
+  return result;
+}
+console.log(addNumbers(12, 122));
